e14d6893f5dcb23bfeff88b4c1f3b4cfa1f65277
FlorentineKlaus <Florentine.Klaus@interia.pl>
2024-06-22 12:14:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -32,10 +32,34 @@ async function browser() {
     }
     catch (err) { }
     await delay(500);
-    await (await page.$("#root > div > div.Layout-sc-1xcs6mc-0.lcpZLv > nav > div > div.Layout-sc-1xcs6mc-0.kuGBVB > div > div > div > div > div.Layout-sc-1xcs6mc-0.jNIlkd > div > div.ScInputWrapper-sc-1fxjr4l-0.drBdyB.tw-combo-input__input > div > div > input")).type(user);
-    await delay(2500);
-    const elements = await page.$$(`[id^="search-result-row"]`);
-    await elements[elements.length - 1].click();
+    let xvv = true;
+    let found = false;
+    let number = 1;
+    while (xvv) {
+        const elementy = await page.$(`#side-nav > div > div.Layout-sc-1xcs6mc-0.capulb > div:nth-child(1) > div.InjectLayout-sc-1i43xsx-0.hWukFy.tw-transition-group > div:nth-child(${number})`);
+        if (elementy) {
+            number++;
+            const innerText = await page.evaluate((el) => el.innerText, elementy);
+            if (innerText.includes(user)) {
+                xvv = false;
+                found = true;
+                await elementy.click();
+            }
+        }
+        else {
+            xvv = false;
+        }
+    }
+    await delay(2000);
+    if (!found) {
+        await (await page.$("#root > div > div.Layout-sc-1xcs6mc-0.lcpZLv > nav > div > div.Layout-sc-1xcs6mc-0.kuGBVB > div > div > div > div > div.Layout-sc-1xcs6mc-0.jNIlkd > div > div.ScInputWrapper-sc-1fxjr4l-0.drBdyB.tw-combo-input__input > div > div > input")).type(user);
+        await delay(2500);
+        const elements = await page.$$(`[id^="search-result-row"]`);
+        await elements[elements.length - 1].click();
+    }
     console.log(await page.url());
+    setInterval(() => {
+        console.log(":3");
+    }, 120000);
 }
 browser();
