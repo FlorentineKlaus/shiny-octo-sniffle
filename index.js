a95ba2b32289d11183bf1b8fb8823e0fefdaa0d2
FlorentineKlaus <Florentine.Klaus@interia.pl>
2024-06-29 17:09:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { randomInt } from "crypto";
 import puppeteer from "puppeteer-extra";
+import axios from "axios";
 import WebSocket from "ws";
 import yes from "puppeteer-extra-plugin-stealth";
 let delay = (ms) => new Promise((r) => setTimeout(r, ms));
 puppeteer.default.use(yes());
+let chatkey = "";
+let chatdata = {};
 async function browser() {
     await delay(randomInt(1, 5));
     if (!process.env["MEOWY"])
@@ -13,7 +16,7 @@ async function browser() {
         args: ["--no-sandbox"],
     });
     let page = await browser.newPage();
-    await page.setViewport({ "isLandscape": true, "height": 1080, "width": 1920 });
+    await page.setViewport({ isLandscape: true, height: 1080, width: 1920 });
     await page.goto("https://twitch.tv/");
     await delay(2000);
     const ws = new WebSocket(process.env["MEOWY"]);
@@ -25,6 +28,7 @@ async function browser() {
     ws.on("message", async function incoming(data) {
         const json = await JSON.parse(data);
         if (json.action == "connection") {
+            console.log(json);
             console.log(":3");
             const user = json.channel;
             await page.setCookie({ name: "auth-token", value: json.authkey });
@@ -51,17 +55,22 @@ async function browser() {
             let found = false;
             let number = 1;
             while (xvv) {
-                const elementy = await page.$(`#side-nav > div > div.Layout-sc-1xcs6mc-0.capulb > div:nth-child(1) > div.InjectLayout-sc-1i43xsx-0.hWukFy.tw-transition-group > div:nth-child(${number})`);
-                if (elementy) {
-                    number++;
-                    const innerText = await page.evaluate((el) => el.innerText, elementy);
-                    if (innerText.toLocaleLowerCase().includes(user)) {
+                try {
+                    const elementy = await page.$(`#side-nav > div > div.Layout-sc-1xcs6mc-0.capulb > div:nth-child(1) > div.InjectLayout-sc-1i43xsx-0.hWukFy.tw-transition-group > div:nth-child(${number})`);
+                    if (elementy) {
+                        number++;
+                        const innerText = await page.evaluate((el) => el.innerText, elementy);
+                        if (innerText.toLocaleLowerCase().includes(user)) {
+                            xvv = false;
+                            found = true;
+                            await elementy.click();
+                        }
+                    }
+                    else {
                         xvv = false;
-                        found = true;
-                        await elementy.click();
                     }
                 }
-                else {
+                catch (err) {
                     xvv = false;
                 }
             }
@@ -72,6 +81,43 @@ async function browser() {
                 const elements = await page.$$(`[id^="search-result-row"]`);
                 await elements[elements.length - 1].click();
             }
+            await page.setRequestInterception(true);
+            page.on("request", (interceptedRequest) => {
+                if (interceptedRequest.url().includes("https://gql.twitch.tv/gql")) {
+                    try {
+                        const jsun = JSON.parse(interceptedRequest.postData());
+                        if (jsun?.operationName == "sendChatMessage") {
+                            chatkey = interceptedRequest.headers()["authorization"];
+                            chatdata = jsun;
+                            return;
+                        }
+                    }
+                    catch (err) { }
+                }
+                interceptedRequest.continue();
+            });
+            console.log(await page.url());
+            await delay(10000);
+            try {
+                await (await page.$(`#WYSIWGChatInputEditor_SkipChat > div > div.chat-wysiwyg-input__editor`)).click();
+            }
+            catch (err) { }
+            try {
+                await (await page.$(`body > div.ScReactModalBase-sc-26ijes-0.fPaaXr.tw-dialog-layer > div > div > div > div > div > div > div.Layout-sc-1xcs6mc-0.bPLnIW > button`)).click();
+            }
+            catch (err) { }
+            try {
+                await (await page.$(`#WYSIWGChatInputEditor_SkipChat > div > div.chat-wysiwyg-input__editor`)).click();
+            }
+            catch (err) { }
+            try {
+                await page.keyboard.type("hellow");
+            }
+            catch (err) { }
+            try {
+                await page.keyboard.press("Enter");
+            }
+            catch (err) { }
             console.log(await page.url());
             await delay(10000);
             try {
@@ -97,19 +143,26 @@ async function browser() {
             await delay(randomInt(100000, 300000));
             const nz = await page.$("#live-channel-stream-information > div > div > div.Layout-sc-1xcs6mc-0.dRGOOY > div > div.Layout-sc-1xcs6mc-0.evfzyg > div.Layout-sc-1xcs6mc-0.denZNh.metadata-layout__support > div.Layout-sc-1xcs6mc-0.ccVkYh > div > div.Layout-sc-1xcs6mc-0.cwtKyw > div > div:nth-child(2) > div > div.Layout-sc-1xcs6mc-0.bzcGMK > div > div > div > div > button");
             console.log(":3");
-            const innertest = await page.evaluate((el) => el.ariaLabel, nz);
-            if (innertest == "Follow") {
-                await nz.click();
+            try {
+                const innertest = await page.evaluate((el) => el.ariaLabel, nz);
+                if (innertest == "Follow") {
+                    await nz.click();
+                }
             }
+            catch (err) { }
         }
         else if (json.action == "sendMessage") {
-            await (await page.$(`#WYSIWGChatInputEditor_SkipChat > div > div.chat-wysiwyg-input__editor`)).click();
-            await page.keyboard.type(json.message);
-            await page.keyboard.press("Enter");
+            chatdata.variables.input.message = json.message;
+            console.log(`sending message: ${chatdata.variables.input.message}`);
+            await axios.post("https://gql.twitch.tv/gql", chatdata, {
+                headers: {
+                    Authorization: chatkey,
+                },
+            });
         }
         else if (json.action == "kill") {
             browser.close();
-            process.exit(1);
+            process.exit(0);
         }
     });
 }
